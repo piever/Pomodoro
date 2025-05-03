5a491b58ad11954b074041b28aac868faba6c85c
piever <pietro.vertechi@protonmail.com>
2025-05-03 20:57:34 +0200

    support pause

--- src/Pomodoro.jl
+++ src/Pomodoro.jl
@@ -1,7 +1,7 @@
 module Pomodoro
 
 using REPL.Terminals: TTYTerminal, raw!
-using Dates: now, Second
+using Dates: now, DateTime, Second
 using Markdown: @md_str, MD, Code
 
 alert() = print("\a")
@@ -56,22 +56,51 @@ const times = [
     "🕦",
 ]
 
-function start_clock(t::Real; interval::Real = 1)
+mutable struct State{T}
+    t0::DateTime
+    dt::Float64
+    ispaused::Bool
+    isfirst::Bool
+    const emojis::T
+end
+
+function State(t::Real)
     t0 = now()
-    isfirst = Ref(true)
+    dt::Float64 = t
+    ispaused = false
+    isfirst = true
     cycled = Iterators.cycle(times)
     emojis = Iterators.Stateful(cycled)
-    return Timer(0; interval) do timer
-        isfirst[] ? (isfirst[] = false) : emoji_backspace()
-        if (now() - t0) / Second(1) < t
-            emoji = popfirst!(emojis)
+    return State(t0, dt, ispaused, isfirst, emojis)
+end
+
+function pause!(state::State)
+    state.dt -= (now() - state.t0) / Second(1)
+    state.ispaused = true
+    return state
+end
+
+function resume!(state::State)
+    state.t0 = now()
+    state.ispaused = false
+    return state
+end
+
+function start_timer(t::Real; interval::Real = 1)
+    state = State(t)
+    timer = Timer(0; interval) do _timer
+        state.ispaused && return
+        state.isfirst ? (state.isfirst = false) : emoji_backspace()
+        if (now() - state.t0) / Second(1) < state.dt
+            emoji = popfirst!(state.emojis)
             print(emoji)
         else
-            close(timer)
+            close(_timer)
             print("🍅")
             alert()
         end
     end
+    return timer, state
 end
 
 function run_app(t::Real; interval::Real = 1)
@@ -80,29 +109,37 @@ function run_app(t::Real; interval::Real = 1)
 
     Press `Enter` to start writing a message.\
     When you are finished, press `Enter` again to start the clock.\
+    Press `p` to pause / restart the clock.
     Press `q` to quit.
     """
     print_md(msg)
 
-    open, timer = true, nothing
+    state, timer = nothing, nothing
 
-    while open
+    while true
         ch = get_char()
-        isticking = !isnothing(timer) && isopen(timer)
+        isrunning = !isnothing(timer) && isopen(timer)
 
-        if !isticking && (ch == '\r')
+        if ch == '\r'
+            isrunning && continue
+
             println()
             println()
             print_md(md"__Task:__")
             print(" ")
             readline()
+
             print_md(md"__Status:__")
             print(" ")
-            timer = start_clock(t; interval)
+
+            timer, state = start_timer(t; interval)
+        elseif lowercase(ch) == 'p'
+            isrunning || continue
+            state.ispaused ? resume!(state) : pause!(state)
         elseif lowercase(ch) == 'q'
-            isticking && close(timer)
+            isrunning && close(timer)
             println()
-            open = false
+            break
         end
     end
 end
